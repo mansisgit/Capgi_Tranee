4086eb2a36f9b10daa5abe03076941eb0282da92
mansisgit <mansicc15@gmail.com>
2026-02-02 19:10:29 +0530

    git

--- src/Solution.java
+++ src/Solution.java
@@ -12,9 +12,45 @@ public class Solution {
 
         if(s=="null" || s.length()==0) return 0;
 
+        int count =0;
 
         String[] words = s.split("\\s+");
 
+        for(String word : words){
+            if(isValid(word)) count++;
+        }
+
+        return count;
     }
+
+    public static boolean isValid(String word){
+        if(word.length()< 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConst = false;
+
+        for(char ch : word.toCharArray()){
+            if(!Character.isLetterOrDigit(ch)) return false;
+
+            if(Character.isLetter(ch)){
+                char lower = Character.toLowerCase(ch);
+
+                if(isVowel(lower)){
+                    hasVowel= true;
+                } else {
+                    hasConst = true;
+                }
+            }
+        }
+        return hasVowel && hasConst;
+    }
+
+    public static boolean isVowel(char c){
+
+        if("aeiouAEIOU".indexOf(c)!=-1) return true;
+
+        return false;
+    }
+
 
 }
